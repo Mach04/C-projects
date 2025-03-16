66dbcc7632c0e1b58bec4d8aaee61f85670baea8
Manuch Imomnazarov <mach04pm@gmail.com>
2025-03-16 11:34:04 +0100

    first c program

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    printf("Hello, World!\n");
+    printf("I am learning C programming,\n\"Softaware developer\"\n");
+    return 0;
+}
+// one line comment
+/* multi 
+line 
+comment */
