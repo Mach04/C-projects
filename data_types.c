cd59b32986ee8461cfe68c61b7200da0bfecb851
Manuch Imomnazarov <mach04pm@gmail.com>
2025-03-19 18:20:47 +0100

    Adding a constant variable

--- data_types.c
+++ data_types.c
@@ -7,16 +7,18 @@ char mychar;
 
 double Mydoublefloat = 15.234354539861;
 
-//type conversion 
 
-int score = 80; 
-float scoref = (float)score; // explicit type conversion
+//this constant variable will stay unchangable;
+const int YEARofBIRTH = 2005;
 
 int main()
 {
-    printf("Double float: %.1f \n", Mydoublefloat); //%.1f only show 1 decimal place
+    //trying to change the constant, but it will lead to an error;
+    YEARofBIRTH = 2002;
+    printf("My year of birth is about to change, but...");
     
-    printf("Score is %.1f", scoref);
+    
+    printf("Double float: %.1f \n", Mydoublefloat); //%.1f only show 1 decimal place
     
     printf("%lu\n", sizeof(myint));
     printf("%lu\n", sizeof(myfloate));
